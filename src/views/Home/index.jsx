262312d1c5412c03f515cb60f0fbb5fea643bcab
Rafael De Leon <rdeleon@okkralabs.com>
2019-03-02 15:39:42 -0400

    Added .env to gitignore. Work in progress home.

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,6 +1,56 @@
-import React from 'react';
+import React, {memo, useEffect, useState} from 'react';
+import CircularProgress from '../../components/CircularProgress';
+
+const LIMIT = 100;
+const URL = process.env.REACT_APP_API_URL;
+const API_KEY = process.env.REACT_APP_API_KEY;
+const INITIAL_STATE = {
+  data: [],
+  meta: {},
+  pagination: {
+    count: 0,
+    offset: 0,
+    total_count: 0,
+  },
+  isFetching: false,
+}
 
 function Home() {
-  return <div />
+  const [gifs, setGifs] = useState(INITIAL_STATE);
+
+  const fetchData = () => {
+    setGifs({
+      ...INITIAL_STATE,
+      isFetching: true,
+    })
+
+    fetch(`${URL}/trending?api_key=${API_KEY}&limit=${LIMIT}`).then((response) => {
+      if (response.status !== 200) {
+        console.log('Error fething gifs. Status Code: ' + response.status);
+        return;
+      }
+
+      response.json().then(function (data) {
+        setGifs({
+          ...data,
+          isFetching: false,
+        });
+      });
+    }).catch((err) => {
+      setGifs(INITIAL_STATE);      
+      console.log('Fetch Error', err);
+    });
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <React.Fragment>
+      <CircularProgress loading={gifs.isFetching}/>
+    </React.Fragment>
+  )
 }
-export default Home
+
+export default memo(Home);
