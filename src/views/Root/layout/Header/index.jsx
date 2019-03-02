a76cdbe8ec17926339bcb93831d0db95c7e97816
Rafael De Leon <rdeleon@okkralabs.com>
2019-03-02 12:49:01 -0400

    Added quick search in header. Fixed header mobile styles.

--- src/views/Root/layout/Header/index.jsx
+++ src/views/Root/layout/Header/index.jsx
@@ -1,12 +1,24 @@
 import './style.scss';
 import React, {memo} from 'react';
 import AppBar from '@material-ui/core/AppBar';
-import gifSVG from '../../../../resources/svg/gif.svg'; 
+import Toolbar from '@material-ui/core/Toolbar';
+import InputBase from '@material-ui/core/InputBase';
+import gifSVG from '../../../../resources/svg/gif.svg';
+import SearchSVG from '../../../../resources/svg/search.svg';
 
 function Home() {
   return (
     <AppBar className="app-header" position="fixed">
-      <img className="header-logo" src={gifSVG} alt="GIF" width="60px"/>
+      <Toolbar>
+        <img className="header-logo" src={gifSVG} alt="GIF" width="60px" />
+        <div className="search-wrapper">
+          <img className="search-icon" src={SearchSVG} alt="Search" width="18px" />
+          <InputBase
+            className="search-input"
+            placeholder="Search..."
+          />
+        </div>
+      </Toolbar>
     </AppBar>
   )
 }
