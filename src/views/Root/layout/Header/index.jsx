f4b154980f3c646602c67dce8cfccc8728a5f316
Rafael De Leon <rdeleon@okkralabs.com>
2019-03-04 14:24:23 -0400

    Added new favicon and logo.

--- src/views/Root/layout/Header/index.jsx
+++ src/views/Root/layout/Header/index.jsx
@@ -2,13 +2,13 @@ import './style.scss';
 import React, {memo} from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import gifSVG from '../../../../resources/svg/gif.svg';
+import Logo from '../../../../resources/svg/logo.svg';
 
 function Header() {
   return (
     <AppBar className="app-header" position="fixed">
       <Toolbar>
-        <img className="header-logo" src={gifSVG} alt="GIF" width="60px" />
+        <img className="header-logo" src={Logo} alt="GIF" width="120px" />
       </Toolbar>
     </AppBar>
   )
